f52d3b8262bcf5c41ad72495ea884f5c4067287f
ljm0726 <l6778@naver.com>
2024-01-11 14:25:48 +0900

    set: setBoot 1

---
+++ src/main/java/com/example/demo/domain/Test/Controller/Controller.java
@@ -0,0 +1,31 @@
+package com.example.demo.domain.Test.Controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/")
+//@CrossOrigin("*") // News 데이터 가져올 때 혹시 체크
+public class Controller {
+
+	@GetMapping("/")
+	public String test() {
+		
+		return "test";
+	}
+	
+	
+	@GetMapping("/test2")
+	public String test2() {
+		
+		return "test2";
+	}
+
+}
